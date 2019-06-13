ef2e0243caffea16a7b02c47241d001ad5ba902f
Jason Bennett <jasoben@gmail.com>
2019-06-13 13:20:34 -0400

    basic working model

---
+++ Femtography Unity/Assets/Scripts/VariableController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableController : MonoBehaviour
+{
+    public GameObject objectToControl, startPosition, endPosition;
+    private float variableToControl;
+    private float distanceMeasure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceMeasure = Vector3.Distance(transform.position, startPosition.transform.position);
+        objectToControl.GetComponent<EllipticalOrbit>().MagnitudeFraction = distanceMeasure * 2;
+    }
+}
