93af9b1394c685350a9a82eac16d10e228063757
Jason Bennett <jasoben@gmail.com>
2019-07-31 09:23:22 -0400

    added words

---
+++ Femtography Unity/Assets/Scripts/Electron Scripts/QuadTextWriter.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuadTextWriter : MonoBehaviour
+{
+
+    public List<TextLines> textLines;
+    private int currentParagraph;
+    public GameObject[] textObjects;
+
+    public float perWordTiming;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentParagraph = 0;
+        StartCoroutine("DisplayText");
+        perWordTiming = .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CueNextText(int nextParagraphNumber = 0)
+    {
+        if (nextParagraphNumber == 0)
+        {
+            currentParagraph = currentParagraph + 1;
+        } else
+        {
+            currentParagraph = nextParagraphNumber;
+        }
+
+    }
+
+    private IEnumerator DisplayText()
+    {
+        int currentLine = 0;
+        string currentText = "currentText";
+        while(true)
+        {
+            for (int i = 0; i < textObjects.Length; i++)
+            {
+                textObjects[i].GetComponent<TextMeshPro>().text = currentText = textLines[currentParagraph].textLine[currentLine];
+            }
+
+            currentLine++;
+
+            if (currentLine + 1 > textLines[currentParagraph].textLine.Count)
+            {
+                yield break;
+            } else
+            {
+                yield return new WaitForSeconds(CountWords(currentText) * perWordTiming);
+            }
+        }
+
+    }
+
+    private int CountWords(string thisString)
+    {
+        int index = 0, wordCount = 0;
+        while (index < thisString.Length && char.IsWhiteSpace(thisString[index]))
+        {
+            index++;
+        }
+
+        while (index < thisString.Length)
+        {
+            while (index < thisString.Length && !char.IsWhiteSpace(thisString[index]))
+            {
+                index++;                
+            }
+
+            wordCount++;
+
+            while (index < thisString.Length && char.IsWhiteSpace(thisString[index]))
+            {
+                index++;
+            }
+
+        }
+
+        return wordCount;
+    }
+}
---
+++ Femtography Unity/Assets/Scripts/Proton Scripts/ProtonTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtonTransform : MonoBehaviour
+{
+    public GameObject electron;
+    public float distanceToElectron;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(gameObject.transform.position, electron.transform.position) > distanceToElectron)
+        {
+            gameObject.transform.Translate(1, 0, 0);
+        }
+
+        
+    }
+}
