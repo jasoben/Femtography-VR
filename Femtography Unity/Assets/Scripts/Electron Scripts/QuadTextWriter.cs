4577d86bfe1386d465e1cb42ed37150054d756a5
Jason Bennett <jasoben@gmail.com>
2019-08-01 08:23:34 -0400

    added text fade and orbiting quarks

--- Femtography Unity/Assets/Scripts/Electron Scripts/QuadTextWriter.cs
+++ Femtography Unity/Assets/Scripts/Electron Scripts/QuadTextWriter.cs
@@ -10,32 +10,40 @@ public class QuadTextWriter : MonoBehaviour
     private int currentParagraph;
     public GameObject[] textObjects;
 
-    public float perWordTiming;
+    public float perWordTiming = .5f;
+
+    private bool fadingOut;
 
     // Start is called before the first frame update
     void Start()
     {
         currentParagraph = 0;
         StartCoroutine("DisplayText");
-        perWordTiming = .5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            CueNextText();
+        }
     }
 
     public void CueNextText(int nextParagraphNumber = 0)
     {
         if (nextParagraphNumber == 0)
         {
-            currentParagraph = currentParagraph + 1;
+            if (currentParagraph + 1 > textLines.Count)
+            {
+            } else { currentParagraph = currentParagraph + 1; }
         } else
         {
             currentParagraph = nextParagraphNumber;
         }
 
+        StopAllCoroutines();
+        StartCoroutine("DisplayText");
     }
 
     private IEnumerator DisplayText()
@@ -57,9 +65,40 @@ public class QuadTextWriter : MonoBehaviour
             } else
             {
                 yield return new WaitForSeconds(CountWords(currentText) * perWordTiming);
+                for (int i = 0; i < textObjects.Length; i++)
+                {
+                    StartCoroutine("FadeBetweenText", textObjects[i].GetComponent<TextMeshPro>());
+                }
+                yield return new WaitWhile(() => fadingOut);
             }
         }
 
+    }
+
+    private IEnumerator FadeBetweenText(TextMeshPro textObject)
+    {
+        byte i = 250;
+        fadingOut = true;
+        bool goingDown = true;
+
+        while(true)
+        {
+            textObject.color = new Color32(255, 255, 255, i);
+            if (goingDown)
+                i = (byte)(i - 2);
+            else
+                i = (byte)(i + 2);
+            if (i < 4)
+            {
+                i = 8;
+                goingDown = false;
+                fadingOut = false;
+            }
+            else if (i > 250)
+            {
+                yield break;
+            } else { yield return new WaitForEndOfFrame(); }
+        }
     }
 
     private int CountWords(string thisString)
---
+++ Femtography Unity/Assets/Scripts/Proton Scripts/QuarkForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuarkForce : MonoBehaviour
+{
+    public Vector3 forceOnQuark;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetComponent<Rigidbody>().AddForce(forceOnQuark);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
