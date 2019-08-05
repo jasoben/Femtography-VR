2e49e3b14f5930932ff1f029ccebbffb15065899
Jason Bennett <jasoben@gmail.com>
2019-08-05 10:02:05 -0400

    proton

---
+++ Femtography Unity/Assets/Scripts/Proton/ProtonController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtonController : MonoBehaviour
+{
+    private Animator protonAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        protonAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeformProton()
+    {
+        protonAnimator.Play("DeformProton", 0, 0);
+    }
+}
