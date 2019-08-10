e624effb21e6a6172d7d934d70775b612013ccc0
Jason Bennett <jasoben@gmail.com>
2019-08-10 12:29:23 -0400

    more ui changes

---
+++ Femtography Unity/Assets/Scripts/UI/UIHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHelper : MonoBehaviour
+{
+    public GameObject helperText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        helperText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowHelperText()
+    {
+        helperText.SetActive(true);
+    }
+    public void HiderHelperText()
+    {
+        helperText.SetActive(false);
+    }
+}
