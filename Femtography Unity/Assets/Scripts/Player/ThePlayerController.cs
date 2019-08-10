edce2f333a01436df5ab96870a8e709596d280a5
Jason Bennett <jasoben@gmail.com>
2019-08-10 11:48:05 -0400

    ui changes

--- Femtography Unity/Assets/Scripts/Player/ThePlayerController.cs
+++ Femtography Unity/Assets/Scripts/Player/ThePlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ThePlayerController : MonoBehaviour
 {
     public Particle particle;
+    public GlobalBool VRorNot;
+
     // Start is called before the first frame update
     void Start()
     {
         particle.speed = 0; 
-        particle.normalSpeed = 1; 
+        particle.normalSpeed = 1;
+        if (XRDevice.isPresent)
+            VRorNot.boolValue = true;
+        else
+            VRorNot.boolValue = false;
     }
 
     // Update is called once per frame
---
+++ Femtography Unity/Assets/Scripts/UI/PointerMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerMover : MonoBehaviour
+{
+
+    private Renderer thisRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRenderer = GetComponent<Renderer>();
+        thisRenderer.enabled = false;
+    }
+
+    public void MakeVisible()
+    {
+        thisRenderer.enabled = true;
+    }
+
+    public void MakeInvisible()
+    {
+        thisRenderer.enabled = false;
+    }
+    
+}
