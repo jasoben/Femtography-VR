43be94b98b7357f19363f3dea7c3a853d97e88ee
Jason Bennett <jasoben@gmail.com>
2019-12-31 16:43:57 -0500

    shader-based photon

---
+++ Femtography Unity/Assets/Scripts/Scene Management/Camera/CustomCameraSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCameraSettings : MonoBehaviour
+{
+    float[] customDistances = new float[32];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 31; i++)
+        { 
+            customDistances[i] = 500; 
+        }
+
+        Camera.main.layerCullDistances = customDistances;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
