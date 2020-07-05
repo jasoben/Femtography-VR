ad7f0102dbe553da69abe8c69a5428315201c521
jasoben <jasoben@gmail.com>
2020-07-05 13:47:22 -0400

    testing collision objects

---
+++ Femtography Unity/Assets/Scripts/Test Scripts/TestMeshCollisionSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMeshCollisionSensor : MonoBehaviour
+{
+    Color hitColor = Color.green;
+    MaterialPropertyBlock materialPropertyBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialPropertyBlock = new MaterialPropertyBlock();
+        materialPropertyBlock.SetColor("_BaseColor", hitColor);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
