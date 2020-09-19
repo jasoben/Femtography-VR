2f7994c65e8ddc802a302dac9b56719d81076577
jasoben <jasoben@gmail.com>
2020-09-19 14:46:22 -0400

    duality view work

---
+++ Femtography Unity/Assets/Scripts/DualityGlass/CloudCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCreator : MonoBehaviour
+{
+    public GameObject cloud;
+    public float spawnTime;
+    public float spawnDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CloudCreatorCoroutine());
+    }
+
+    IEnumerator CloudCreatorCoroutine()
+    {
+        GameObject newCloud = Instantiate(cloud, transform);
+        newCloud.transform.localPosition = new Vector3(Random.Range(-spawnDistance, spawnDistance), Random.Range(-spawnDistance, spawnDistance), Random.Range(-spawnDistance, spawnDistance));
+        float spawnSize = Random.Range(1f, 2f);
+        newCloud.transform.localScale = new Vector3(spawnSize, spawnSize, spawnSize);
+        Invoke("StartCoroutine", spawnTime);
+        yield break;
+    }
+
+    void StartCoroutine()
+    {
+        StartCoroutine(CloudCreatorCoroutine());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Femtography Unity/Assets/Scripts/DualityGlass/CloudRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudRandomizer : MonoBehaviour
+{
+    MaterialPropertyBlock materialPropertyBlock;
+    Vector3 rotationAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialPropertyBlock = new MaterialPropertyBlock();
+        materialPropertyBlock.SetVector("RandomSeed_", new Vector4(Random.Range(0,10), Random.Range(0,10), 0, 0));
+        GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
+        Invoke("DestroyCloud", 2);
+        rotationAxis = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(transform.parent.position, rotationAxis, 1f);
+    }
+
+    void DestroyCloud()
+    {
+        Destroy(gameObject);
+    }
+}
