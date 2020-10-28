e01b0dbda9c4f5192704a5ef53f851ddbed7e44d
jasoben <jasoben@gmail.com>
2020-10-28 13:04:43 -0400

    Interference screen enabled

---
+++ Femtography Unity/Assets/Scripts/Camera/RotateAroundObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour
+{
+    public GameObject centerObject;
+    public float rotateAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(centerObject.transform.position, Vector3.up, rotateAmount);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.RotateAround(centerObject.transform.position, Vector3.up, -rotateAmount);
+        }
+        
+    }
+}
--- Femtography Unity/Assets/Scripts/Interference Patterns/MeshVertexTracker.cs
+++ Femtography Unity/Assets/Scripts/Interference Patterns/MeshVertexTracker.cs
@@ -6,8 +6,8 @@ using WaveMaker;
 
 public class MeshVertexTracker : MonoBehaviour
 {
-    Vector3[] vertices, highestVerts;
-    WaveMakerSurface waveMakerSurface;
+    Vector3[] vertices, highestVerts, lowestVerts;
+    public WaveMakerSurface waveMakerSurface;
     Mesh mesh;
 
     List<int> nearbyVertices = new List<int>();
@@ -15,11 +15,14 @@ public class MeshVertexTracker : MonoBehaviour
 
     public float zPos;
     public float bufferSize;
+    public float zMovementAmount;
+    public GameObject recalibratingText;
+
     // Start is called before the first frame update
     void Start()
     {
-        waveMakerSurface = GetComponent<WaveMakerSurface>();
         StartCoroutine(CheckInit());
+        StartCoroutine(FindHighestVerts());
     }
 
     IEnumerator CheckInit()
@@ -41,6 +44,12 @@ public class MeshVertexTracker : MonoBehaviour
 
     bool DefineRegion()
     {
+        zPos = transform.position.z;
+
+        nearbyVertices.Clear();
+
+        mesh = waveMakerSurface.Mesh_; // The mesh is constantly updated, so we need to keep it updated here
+
         for (int i = 0; i < mesh.vertices.Length; i++)
         {
             if (Mathf.Abs(zPos - mesh.vertices[i].z) < bufferSize)
@@ -48,22 +57,70 @@ public class MeshVertexTracker : MonoBehaviour
                 nearbyVertices.Add(i);
             }
         }
-        
-        highestVerts = new Vector3[nearbyVertices.Count];
 
-        for (int i = 0; i < nearbyVertices.Count; i++)
-        {
-            highestVerts[i] = vertices[nearbyVertices[i]];
-        }
+        vertices = mesh.vertices;
+        highestVerts = new Vector3[vertices.Length];
+        lowestVerts = new Vector3[vertices.Length];
+
+        recalibratingText.SetActive(false);
 
         return true;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Mathf.Abs(transform.position.z) > Mathf.Abs(zPos) + .1f ||
+        //    Mathf.Abs(transform.position.z) < Mathf.Abs(zPos) - .1f) // If it moves out of range, we redefine the region
+        //    // we wish to examine
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            recalibratingText.SetActive(true);
+            zPos = transform.position.z;
+            if (regionDefined)
+                Invoke("DefineRegion", .1f);
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.position += new Vector3(0, 0, zMovementAmount);
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            transform.position -= new Vector3(0, 0, zMovementAmount);
+            if (transform.position.z < 0)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            }
+        }
     }
 
+    IEnumerator FindHighestVerts()
+    {
+        while (true)
+        {
+            if (regionDefined)
+            {
+                foreach (int nearbyVertex in nearbyVertices)
+                {
+                    if (vertices[nearbyVertex].y > highestVerts[nearbyVertex].y) 
+                    {
+                        highestVerts[nearbyVertex] = vertices[nearbyVertex];
+                    }
+                    if (vertices[nearbyVertex].y < lowestVerts[nearbyVertex].y)
+                    {
+                        lowestVerts[nearbyVertex] = vertices[nearbyVertex];
+                    }
+                }
+
+
+                yield return new WaitForSecondsRealtime(.5f);
+
+                mesh = waveMakerSurface.Mesh_;
+                vertices = mesh.vertices;
+
+            }
+        }
+    }
     private void OnDrawGizmos()
     {
         if (regionDefined)
@@ -74,22 +131,20 @@ public class MeshVertexTracker : MonoBehaviour
             {
                 Gizmos.color = Color.green;
                 Gizmos.DrawSphere(vertices[vertexNumber], .2f);
-            }
 
-            for (int i = 0; i < highestVerts.Length; i++)
-            {
-                if (highestVerts[i].y < vertices[nearbyVertices[i]].y)
-                {
-                    highestVerts[i] = vertices[nearbyVertices[i]];
-                }
                 Gizmos.color = Color.red;
-                Gizmos.DrawSphere(highestVerts[i], .2f);
+                Gizmos.DrawSphere(highestVerts[vertexNumber], .2f);
+
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(lowestVerts[vertexNumber], .2f);
             }
         }
     }
 
     private void OnValidate()
     {
-        DefineRegion();
+        zPos = transform.position.z;
+        if (regionDefined)
+            DefineRegion();
     }
 }
