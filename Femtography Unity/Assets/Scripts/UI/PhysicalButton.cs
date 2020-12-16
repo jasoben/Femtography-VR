70f513d11bb7a5a1abc45f09f1e28f0e78e245b8
jasoben <jasoben@gmail.com>
2020-12-15 19:12:44 -0500

    ui is beautiful

--- Femtography Unity/Assets/Scripts/UI/PhysicalButton.cs
+++ Femtography Unity/Assets/Scripts/UI/PhysicalButton.cs
@@ -1,15 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhysicalButton : MonoBehaviour
 {
-    public Material mouseOverMaterial;
-    private Material startMaterial;
+    public Text UIText;
+    public Color regularTextColor, highlightTextColor, regularButtonColor, highlightButtonColor;
+
+    private Color startFadeButtonColor, endFadeButtonColor, startFadeTextColor, endFadeTextColor,
+        currentFadingButtonColor, currentFadingTextColor;
+    MaterialPropertyBlock shiftingColor;
+
+    public float regularAlphaAmount, highlightAlphaAmount;
+
+    private float startAlpha, endAlpha, currentAlpha;
+
+    float fadeCounter = 0;
+
+    public float fadeSpeed;
+
+    bool canFadeIn = true, canFadeOut, isFadingIn;
+
+    IEnumerator FadeInCoroutine, FadeOutCoroutine;
+
     // Start is called before the first frame update
     void Start()
     {
-        startMaterial = GetComponent<Renderer>().material;
+        shiftingColor = new MaterialPropertyBlock();
+        currentFadingButtonColor = regularButtonColor;// look to the Coroutine FadeUI to see why we set these here
+        currentFadingTextColor = regularTextColor;
+        currentAlpha = regularAlphaAmount;
+
+        if (UIText != null)
+            UIText.color = regularTextColor;
     }
 
     // Update is called once per frame
@@ -18,13 +42,70 @@ public class PhysicalButton : MonoBehaviour
         
     }
 
-    private void OnMouseOver()
+    IEnumerator FadeUI()
     {
-        GetComponent<Renderer>().material = mouseOverMaterial;
-        Debug.Log("mouse over");
+        fadeCounter = 0;
+        while (true)
+        {
+            if (isFadingIn)
+            {
+                startFadeButtonColor = currentFadingButtonColor;
+                endFadeButtonColor = highlightButtonColor;
+
+                startFadeTextColor = currentFadingTextColor;
+                endFadeTextColor = highlightTextColor;
+
+                startAlpha = currentAlpha;
+                endAlpha = highlightAlphaAmount;
+            }
+            else if (!isFadingIn)
+            {
+                startFadeButtonColor = currentFadingButtonColor;
+                endFadeButtonColor = regularButtonColor;
+
+                startFadeTextColor = currentFadingTextColor;
+                endFadeTextColor = regularTextColor;
+
+                startAlpha = currentAlpha;
+                endAlpha = regularAlphaAmount;
+            }
+
+            currentFadingTextColor = Color.Lerp(startFadeTextColor, endFadeTextColor, fadeCounter);
+            currentFadingButtonColor = Color.Lerp(startFadeButtonColor, endFadeButtonColor, fadeCounter);
+            currentAlpha = Mathf.Lerp(startAlpha, endAlpha, fadeCounter);
+
+            shiftingColor.SetColor("Color_", currentFadingButtonColor);
+            shiftingColor.SetColor("GlowColor", currentFadingButtonColor);
+            shiftingColor.SetFloat("Alpha_", currentAlpha);
+
+            GetComponent<Renderer>().SetPropertyBlock(shiftingColor);
+            if (UIText != null)
+                UIText.color = currentFadingTextColor;
+
+            fadeCounter += fadeSpeed;
+
+            if (fadeCounter > 1)
+            {
+                yield break;
+            } else
+                yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        isFadingIn = true;
+        FadeInCoroutine = FadeUI();
+        if (FadeOutCoroutine != null)
+            StopCoroutine(FadeOutCoroutine);
+        StartCoroutine(FadeInCoroutine);
     }
     private void OnMouseExit()
     {
-        GetComponent<Renderer>().material = startMaterial;
+        isFadingIn = false;
+        FadeOutCoroutine = FadeUI();
+        if (FadeInCoroutine != null)
+            StopCoroutine(FadeInCoroutine);
+        StartCoroutine(FadeOutCoroutine);
     }
 }
--- Femtography Unity/Assets/Scripts/UI/UIHelper.cs
+++ Femtography Unity/Assets/Scripts/UI/UIHelper.cs
@@ -6,9 +6,6 @@ using UnityEngine.EventSystems;
 
 public class UIHelper : MonoBehaviour
 {
-    public bool highlightButton { get; set; }
-    Color baseColor, flashingColor, newColor, regularColor, highlightColor;
-    ColorBlock toggleColorBlock, buttonColorBlock;
     public MenuManagerObject menuManagerObject;// Sometimes the menu is active and sometimes it isn't, so we enable
     // and disable objects through ScriptableObject data instead of directly. See the "MenuManager" under "Control Objects" 
     // in the hierarchy.
@@ -19,24 +16,6 @@ public class UIHelper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (GetComponent<Image>() != null)
-            baseColor = GetComponent<Image>().color;
-        else
-            baseColor = Color.white;
-        flashingColor = Color.gray;
-        newColor = new Color();
-        if (GetComponent<Image>() != null)
-            image = GetComponent<Image>();
-        if (GetComponent<Toggle>() != null)
-        {
-            toggle = GetComponent<Toggle>();
-            toggleColorBlock = toggle.colors;
-        }
-
-        highlightColor = new Color(255/255f, 241/255f, 0, .7f);
-        regularColor = new Color(124/255f, 144/255f, 191/255f, .7f);
-        //regularColor = Color.blue;
-
         toggleTooltip = GetComponentInChildren<ToggleTooltip>();
 
         // We define these OnPointer events in code since we have so many objects that use them, and it would be inefficient to set them all
@@ -72,34 +51,8 @@ public class UIHelper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (highlightButton)
-        {
-            newColor = Color.Lerp(highlightColor, flashingColor, FlashingController.FlashLerp);
-            if (image != null)
-                image.color = newColor;
-            if (toggle != null)
-            {
-                toggleColorBlock.normalColor = newColor;
-                toggle.colors = toggleColorBlock;
-                toggle.transform.Find("Label").gameObject.GetComponent<Text>().color = newColor;
-            }
-        }
-        else if (!highlightButton)
-        {
-            if (image != null)
-                image.color = baseColor;
-            if (toggle != null)
-            {
-                toggleColorBlock.normalColor = baseColor;
-                toggle.colors = toggleColorBlock;
-                toggle.transform.Find("Label").gameObject.GetComponent<Text>().color = baseColor;
-            }
-        }
-
         if (toggle != null)
             toggle.interactable = menuManagerObject.isActive;
-
-        highlightButton = menuManagerObject.isFlashing;
     }
 
     public void ToggleOnOrOff(bool onOrOff)
