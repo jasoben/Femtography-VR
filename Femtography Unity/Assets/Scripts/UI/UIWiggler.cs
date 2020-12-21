8f0595474d8f1b6089d5911b11d15d78fd1dfe25
jasoben <jasoben@gmail.com>
2020-12-21 18:54:27 -0500

    trying to fix the wiggler

--- Femtography Unity/Assets/Scripts/UI/UIWiggler.cs
+++ Femtography Unity/Assets/Scripts/UI/UIWiggler.cs
@@ -6,32 +6,25 @@ public class UIWiggler : MonoBehaviour
 {
     // This wiggles UI around a bit to preserve the appearance of the liquid display
 
-    float wiggleAmountX = .00002f, wiggleAmountY = .00002f, totalWiggleSpanY = .005f, totalWiggleSpanX = .007f, currentPositionX, currentPositionY;
+    public Vector3 wiggleSpan, wiggleAmount, wiggleRange;
+    Vector3 startPosition;
+    public float wiggleCoefficient;
     // Start is called before the first frame update
     void Start()
     {
-        currentPositionX = Random.Range(-.01f, .01f);
-        currentPositionY = Random.Range(-.01f, .01f);
+        wiggleRange = wiggleRange * wiggleCoefficient;
+        wiggleAmount = wiggleAmount * wiggleCoefficient;
+        wiggleSpan = wiggleSpan * wiggleCoefficient;
+
+        Vector3 randomStartPosition = new Vector3(Random.Range(-wiggleRange.x, wiggleRange.x), Random.Range(-wiggleRange.y, wiggleRange.y),
+            Random.Range(-wiggleRange.z, wiggleRange.z));
+        transform.position = transform.position + randomStartPosition;
+        startPosition = randomStartPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(wiggleAmountX, wiggleAmountX, 0);
-        currentPositionX += wiggleAmountX;
-        currentPositionY += wiggleAmountY;
-        
-        if (Mathf.Abs(currentPositionX) > totalWiggleSpanX)
-        {
-            wiggleAmountX = -wiggleAmountX;
-            if (wiggleAmountX > 0)
-                wiggleAmountX = Random.Range(.000005f, .00002f);
-        }
-        if (Mathf.Abs(currentPositionY) > totalWiggleSpanY)
-        {
-            wiggleAmountY = -wiggleAmountY;
-            if (wiggleAmountY > 0)
-                wiggleAmountY = Random.Range(.000005f, .00002f);
-        }
+        transform.position += new Vector3(wiggleAmount.x, wiggleAmount.y, wiggleAmount.z);
     }
 }
