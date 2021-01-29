8895534921c7380cd48e16fb5dc76981a5e7f4be
jasoben <jasoben@gmail.com>
2021-01-28 21:18:43 -0500

    Revert "Revert "making sliders""
    This reverts commit f8fde4721e96333ad007da79852a0e8a877dce7d.

---
+++ Femtography Unity/Assets/Scripts/VisualFX/BackGroundTimeDistort.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundTimeDistort : MonoBehaviour
+{
+    Material backgroundMaterial;
+    public FloatReference simSpeed;
+    float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = currentTime + simSpeed.Value * Time.deltaTime;
+        backgroundMaterial.SetFloat("Time_", currentTime); // .1f because we want it to be slow even at max speed
+    }
+}
