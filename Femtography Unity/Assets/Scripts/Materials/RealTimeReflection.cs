a3fa74081ddfdf2508d23f11f746f9301c2d38f2
jasoben <jasoben@gmail.com>
2021-06-02 21:31:41 -0400

    updated museum

---
+++ Femtography Unity/Assets/Scripts/Materials/RealTimeReflection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealTimeReflection : MonoBehaviour
+{
+    ReflectionProbe reflectionProbe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reflectionProbe = GetComponent<ReflectionProbe>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reflectionProbe.RenderProbe();
+    }
+}
