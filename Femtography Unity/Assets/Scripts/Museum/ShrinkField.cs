f54ad7b85f70d54878d070725c0a018b3519cd5b
jasoben <jasoben@gmail.com>
2021-06-04 13:17:27 -0400

    working on shrink field

---
+++ Femtography Unity/Assets/Scripts/Museum/ShrinkField.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkField : MonoBehaviour
+{
+    GameObject player;
+    public float shrinkSpeed;
+
+    public GameObject growRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        player = other.gameObject;
+
+        StartCoroutine(ShrinkCharacter());
+    }
+
+    IEnumerator ShrinkCharacter()
+    {
+        float shrinkage = 0;
+        Vector3 playerSize = player.transform.localScale;
+        float cameraField = Camera.main.fieldOfView;
+
+        Vector3 roomSize = growRoom.transform.localScale;
+        Vector3 roomFinalSize = growRoom.transform.localScale * 100;
+
+        player.SetActive(false);
+        growRoom.SetActive(true);
+
+        while (true)
+        {
+            shrinkage += shrinkSpeed;
+
+            growRoom.transform.localScale = Vector3.Lerp(roomSize, roomFinalSize, shrinkage);
+
+            if (shrinkage > 1)
+                yield break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
