fba4e8332c714dd45e1fe10ee59c20faae83c595
jasoben <jasoben@gmail.com>
2022-05-07 14:50:24 -0400

    working on xr interaction

---
+++ Femtography Unity/Assets/Scripts/Museum/DisableStartingLights.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class DisableStartingLights : MonoBehaviour
+{
+    public GameEvent startingLightsOff;
+    GameObject[] startingLights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingLights = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            startingLights[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public async void PlaySoundDisableLights()
+    {
+        for (int i = 0; i < startingLights.Length; i++)
+        {
+            startingLights[i].GetComponent<Renderer>().enabled = false;
+            startingLights[i].GetComponentInChildren<Light>().enabled = false;
+            startingLights[i].GetComponent<AudioSource>().Play();
+            await Task.Delay(3000);
+            startingLights[i].SetActive(false);
+        }
+        await Task.Delay(2000);
+        startingLightsOff.Raise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
