41fe93fd911cc80a16670049b30b4814f9131f83
jasoben <jasoben@gmail.com>
2023-07-10 11:03:30 -0400

    updated floor tiles to be clickable

--- Femtography Unity/Assets/Scripts/Navigation/TeleportAnchorNONXR.cs
+++ Femtography Unity/Assets/Scripts/Navigation/TeleportAnchorNONXR.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class TeleportAnchorNONXR : MonoBehaviour
+public class TeleportAnchorNONXR : MonoBehaviour, IPointerClickHandler
 {
+    GameObject playerObject;
+
+    [SerializeField] UnityEvent onClick;
+
+    PointerEventData pointerEventData;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +26,18 @@ public class TeleportAnchorNONXR : MonoBehaviour
 
     public void TeleportPlayerToLocation()
     {
+        playerObject = pointerEventData.pressEventCamera.gameObject;
 
+        playerObject.transform.position = (new Vector3(
+            this.transform.position.x,
+            playerObject.transform.position.y,
+            this.transform.position.z
+            ));
+    }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        pointerEventData = eventData;
+        onClick.Invoke();
     }
 }
