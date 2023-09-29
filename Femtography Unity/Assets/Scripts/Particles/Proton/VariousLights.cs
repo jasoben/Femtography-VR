b8c03975c24f6df978e39beea6b6e6bd08f471e0
jasoben <jasoben@gmail.com>
2023-09-29 08:19:52 -0400

    fixed lights from proton

--- Femtography Unity/Assets/Scripts/Particles/Proton/VariousLights.cs
+++ Femtography Unity/Assets/Scripts/Particles/Proton/VariousLights.cs
@@ -12,35 +12,39 @@ public class VariousLights : MonoBehaviour
     [SerializeField] float intensityRange;
     float randomIntensity;
     float lerpAmount = 0;
+    bool lerpGoingUp = true;
     // Start is called before the first frame update
     void Start()
     {
         lights = new Light[] {light1, light2, light3};
         randomIntensity = Random.Range(intensity - intensityRange, intensity + intensityRange);
+        light1.range = lightSize + 5; 
+        light2.range = lightSize + 10; 
+        light3.range = lightSize + 15; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        light1.range = lightSize + 5; 
-        light2.range = lightSize + 10; 
-        light3.range = lightSize + 15; 
 
         foreach (Light light in lights)
         {
-            lerpAmount += flickerSpeed;
+            lerpAmount += flickerSpeed * Time.deltaTime;
             light.intensity = Mathf.Lerp(intensity, randomIntensity, lerpAmount);
         }
+        Debug.Log(lerpAmount);
         
-        if (lerpAmount > 1)
+        if (lerpAmount > 1 && lerpGoingUp)
         {
             flickerSpeed = -flickerSpeed;
+            lerpGoingUp = false;
         }
 
-        else if (lerpAmount < 0)
+        else if (lerpAmount < 0 && !lerpGoingUp)
         {
             randomIntensity = Random.Range(intensity - intensityRange, intensity + intensityRange);
             flickerSpeed = -flickerSpeed;
+            lerpGoingUp = true;
         }
     }
 }
